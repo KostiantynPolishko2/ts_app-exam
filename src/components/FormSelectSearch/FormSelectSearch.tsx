5e9b5bbf1806a79b9d1deab6da6bc0d8fbf8fef0
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2024-07-06 15:13:59 +0300

    2 Has created component FormSelectSearch
    Testing of node_module react-select-search

---
+++ src/components/FormSelectSearch/FormSelectSearch.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+import SelectSearch from 'react-select-search';
+import { FormSelectSearchWrapper } from './FormSelectSearch.styled';
+
+interface FormSelectSearchProps {}
+
+const FormSelectSearch: FC<FormSelectSearchProps> = () => (
+ <FormSelectSearchWrapper>
+    FormSelectSearch Component
+ </FormSelectSearchWrapper>
+);
+
+export default FormSelectSearch;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/App/App';
+import FormSelectSearch from './components/FormSelectSearch/FormSelectSearch';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <FormSelectSearch />
   </React.StrictMode>
 );
 
