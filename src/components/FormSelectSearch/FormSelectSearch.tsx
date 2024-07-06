6e21cba6d6d90b5e0c7b895ef9cd91c754e70dae
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2024-07-06 23:01:07 +0300

    3 Has installed, tested node module 'react-select-search'
    'react-select-search' gives possibility create list of selected value from list.

--- src/components/FormSelectSearch/FormSelectSearch.tsx
+++ src/components/FormSelectSearch/FormSelectSearch.tsx
@@ -1,13 +1,38 @@
-import React, { FC } from 'react';
-import SelectSearch from 'react-select-search';
+import React, { FC, useState } from 'react';
+import SelectSearch, { SelectedOption, SelectedOptionValue } from 'react-select-search';
+import 'react-select-search/style.css';
+import './FormSelectSearch.css';
 import { FormSelectSearchWrapper } from './FormSelectSearch.styled';
 
 interface FormSelectSearchProps {}
 
-const FormSelectSearch: FC<FormSelectSearchProps> = () => (
- <FormSelectSearchWrapper>
-    FormSelectSearch Component
- </FormSelectSearchWrapper>
-);
+const getSelection = (selectedValue: SelectedOptionValue | SelectedOptionValue[]): void => {
+   console.log('selected value', selectedValue);
+}
 
+const FormSelectSearch: FC<FormSelectSearchProps> = () => {
+
+   const [select, setSelect] = useState<string>('none');
+
+   const handleSelect = (selectedValue: SelectedOptionValue | SelectedOptionValue[]): void => {
+      setSelect(selectedValue.toLocaleString());
+   }
+
+
+   const options = [
+      {name: 'Swedish', value: 'sv'},
+      {name: 'English', value: 'en'},
+      {name: 'Ukraine', value: 'ua'},
+      {name: 'Spanish', value: 'es'},
+      {name: 'Italian', value: 'it'}
+   ]
+
+   return (
+      <FormSelectSearchWrapper>
+         FormSelectSearch Component
+         <p>Selecte value <span style={{backgroundColor: 'red'}}>{select}</span></p>
+         <SelectSearch options={options} onChange={handleSelect} placeholder='Choose your language'/>
+      </FormSelectSearchWrapper>
+     );  
+}
 export default FormSelectSearch;
