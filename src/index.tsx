ed20d754548032174076117d9f6fa5bb1ad3390e
Kostiantyn Polishko <167745085+KostiantynPolishko2@users.noreply.github.com>
2024-07-09 13:48:16 +0300

    FC Header release v1.0.0 (#1)
    * 2 Created FC Header
    
    * 3 Header styled-componet set upped for center position inside of all rest FC

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/App/App';
+import Header from './components/Header/Header';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Header/>
   </React.StrictMode>
 );
 
