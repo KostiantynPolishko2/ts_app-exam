b56b78afeede2e4a135f0e40c8d67c4bb91dbaae
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2024-07-09 22:58:14 +0300

    5 Has added funcction for handle inputed value

--- src/components/FormSearch/FormSearch.tsx
+++ src/components/FormSearch/FormSearch.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 import { FormSearchWrapper } from './FormSearch.styled';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './FormSearch.css';
+import { getDigits, handleInput } from './FormFunction';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Stack from 'react-bootstrap/Stack';
@@ -17,8 +18,10 @@ const FormSearch: FC<FormSearchProps> = (props): React.FunctionComponentElement<
       <Form.Group controlId='search_auto'>
          <Form.Label className='form'>Поиск авто по {props.labelTxt || 'номеру'}</Form.Label>
          <Stack direction='horizontal' gap={0}>
-            <Form.Control type='text' placeholder={props.placeholderTxt || 'Номерной знак'} className='form'/>
-            <Button name='search' type='button' form='search_auto' variant='primary' className='form'>Поиск</Button>
+            <Form.Control type='text' placeholder={props.placeholderTxt || 'Номерной знак'} className='form'
+            onKeyDown={handleInput}/>
+            <Button name='search' type='button' form='search_auto' variant='primary' className='form'
+            onClick={getDigits}>Поиск</Button>
          </Stack>
       </Form.Group>
     </Form>
