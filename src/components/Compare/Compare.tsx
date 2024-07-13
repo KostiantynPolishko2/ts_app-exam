519b6250590dae45ad16f7c9e8a5e5a5134a6321
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2024-07-13 14:53:44 +0300

    3 Has managed simple axios request for get full_title

--- src/components/Compare/Compare.tsx
+++ src/components/Compare/Compare.tsx
@@ -1,12 +1,49 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import axios from 'axios';
 import { CompareWrapper } from './Compare.styled';
 
-interface CompareProps {}
+interface CompareProps {
+   modelMark: string,
+}
 
-const Compare: FC<CompareProps> = () => (
- <CompareWrapper>
-    Compare Component
- </CompareWrapper>
-);
+interface IModelMark {
+   full_tittle: string,
+}
+
+
+const Compare: FC<CompareProps> = ({modelMark}) => {
+
+   const [tittle, setTittle] = useState<string>();
+
+   const fetchDataModelMark = async (modelMark: string): Promise<void> => {
+      const key = '349a1acb991de77d8d42744d27b6f379';
+   
+      modelMark = modelMark.replace(' ', '/');
+   
+      const client = axios.create({
+         baseURL: 'https://baza-gai.com.ua/make/',
+         headers: {'Accept': 'application/json', 'X-Api-Key': key},
+         method: 'get',
+         responseType: 'json',
+      });
+   
+      try{
+         const responce = (await client.get(modelMark));
+         console.log(responce.data['full_title']);
+         setTittle(responce.data['full_title']);
+      }
+      catch (error){
+         setTittle('');
+      }
+   }
+
+   return (
+      <CompareWrapper>
+         <p>Compare</p>
+         <p>Full tittle: {tittle}</p>
+         <button type='button' onClick={() => {fetchDataModelMark(modelMark)}}>Compare </button>
+      </CompareWrapper>
+     );
+}
 
 export default Compare;
--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Compare />
+    <Compare modelMark={'bmw x3'}/>
   </React.StrictMode>
 );
 
