84eeb434e943d7a2b52c0b3446ade6280de8a8a5
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2024-07-13 16:14:19 +0300

    4 Has done logic event onLoad by axios reqeust.

--- src/components/Compare/Compare.tsx
+++ src/components/Compare/Compare.tsx
@@ -1,19 +1,35 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import axios from 'axios';
 import { CompareWrapper } from './Compare.styled';
 
 interface CompareProps {
    modelMark: string,
-}
+};
+
+interface ICatalogModel {
+   photo_url: string,
+   price_avg: number,
+   price_max: number,
+   price_min: number,
+   year_from: number,
+};
 
 interface IModelMark {
-   full_tittle: string,
-}
+   catalog_model: ICatalogModel,
+   full_title: string,
+   id: number,
+};
 
+interface IError {
+   status: number
+   code: string,
+   name: string
+};
 
 const Compare: FC<CompareProps> = ({modelMark}) => {
 
-   const [tittle, setTittle] = useState<string>();
+   const [dataModelMark, setDataModelMark] = useState<IModelMark | null >(null);
+   const [errorModelMark, setErrorModelMark] = useState<IError | null>(null);
 
    const fetchDataModelMark = async (modelMark: string): Promise<void> => {
       const key = '349a1acb991de77d8d42744d27b6f379';
@@ -29,19 +45,25 @@ const Compare: FC<CompareProps> = ({modelMark}) => {
    
       try{
          const responce = (await client.get(modelMark));
-         console.log(responce.data['full_title']);
-         setTittle(responce.data['full_title']);
+         setDataModelMark(responce.data);
       }
       catch (error){
-         setTittle('');
+         setErrorModelMark({status: 404, code: 'ERR_BAD_REQUEST', name: 'AxiosError'});
       }
    }
 
+   useEffect(() => {fetchDataModelMark(modelMark);}, [modelMark]);
+
    return (
-      <CompareWrapper>
+      <CompareWrapper onLoad={e => {fetchDataModelMark(modelMark)}}>
          <p>Compare</p>
-         <p>Full tittle: {tittle}</p>
-         <button type='button' onClick={() => {fetchDataModelMark(modelMark)}}>Compare </button>
+         <p>Full tittle: {dataModelMark?.full_title}</p>
+         <p>Id: {dataModelMark?.id}</p>
+         <p>photo_url: {dataModelMark?.catalog_model.photo_url}</p>
+         <p>price_avg: {dataModelMark?.catalog_model.price_avg}</p>
+         <p>price_max: {dataModelMark?.catalog_model.price_max}</p>
+         <p>price_min: {dataModelMark?.catalog_model.price_min}</p>
+         <p>year_from: {dataModelMark?.catalog_model.year_from}</p>
       </CompareWrapper>
      );
 }
