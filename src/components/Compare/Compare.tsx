d9ed6c4ffff78d482acc4b66ef4c884148ee4c1b
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2024-07-13 14:32:27 +0300

    2 Has created template FC Compare

---
+++ src/components/Compare/Compare.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import { CompareWrapper } from './Compare.styled';
+
+interface CompareProps {}
+
+const Compare: FC<CompareProps> = () => (
+ <CompareWrapper>
+    Compare Component
+ </CompareWrapper>
+);
+
+export default Compare;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/App/App';
+import Compare from './components/Compare/Compare';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Compare />
   </React.StrictMode>
 );
 
